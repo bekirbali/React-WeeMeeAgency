fd71a0c296e43999fad0f8ace8d50b447500a38d
bekirbali <bfbali43@gmail.com>
2025-01-24 15:51:41 +0300

    Add FAQ content and enhance navigation with new logo
    - Expanded the FAQ component with additional questions and answers related to SEO and digital marketing, improving user information access.
    - Integrated new logo into the Navbar for branding consistency.
    - Updated localization files in English and Turkish to reflect the new FAQ content and ensure comprehensive language support.
    - Enhanced navigation functionality using React Router to improve user experience.

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,6 @@ import alarga from "./alarga.jpeg";
 import sorbil from "./sorbilpng.png";
 import suzenia from "./suzeniapng.png";
 import hero_img from "./hero_img.jpeg";
+import weemeelogo from "./weemeelogo.png";
 
-export { alarga, sorbil, suzenia, hero_img };
+export { alarga, sorbil, suzenia, hero_img, weemeelogo };
--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,42 +1,148 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useTranslation } from "react-i18next";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const FAQ = () => {
   const { t } = useTranslation();
   const [openIndex, setOpenIndex] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const faqItems = [
     {
-      key: "services",
-      question: t("faq.items.services.question"),
-      answer: t("faq.items.services.answer"),
+      key: "seo-nedir",
+      question: t("faq.items.seo-nedir.question"),
+      answer: t("faq.items.seo-nedir.answer"),
     },
     {
-      key: "timeline",
-      question: t("faq.items.timeline.question"),
-      answer: t("faq.items.timeline.answer"),
+      key: "seo-calismalari-nelerdir",
+      question: t("faq.items.seo-calismalari-nelerdir.question"),
+      answer: t("faq.items.seo-calismalari-nelerdir.answer"),
     },
     {
-      key: "international",
-      question: t("faq.items.international.question"),
-      answer: t("faq.items.international.answer"),
+      key: "seo-calismalari-ne-kadar-surede-sonuc-verir",
+      question: t(
+        "faq.items.seo-calismalari-ne-kadar-surede-sonuc-verir.question"
+      ),
+      answer: t("faq.items.seo-calismalari-ne-kadar-surede-sonuc-verir.answer"),
     },
     {
-      key: "pricing",
-      question: t("faq.items.pricing.question"),
-      answer: t("faq.items.pricing.answer"),
+      key: "seo-hizmeti-almanin-faydalari",
+      question: t("faq.items.seo-hizmeti-almanin-faydalari.question"),
+      answer: t("faq.items.seo-hizmeti-almanin-faydalari.answer"),
     },
     {
-      key: "existing",
-      question: t("faq.items.existing.question"),
-      answer: t("faq.items.existing.answer"),
+      key: "web-sitesi-analizi-neden-onemlidir",
+      question: t("faq.items.web-sitesi-analizi-neden-onemlidir.question"),
+      answer: t("faq.items.web-sitesi-analizi-neden-onemlidir.answer"),
+    },
+    {
+      key: "web-sitesi-analizi-kapsaminda-neler-yapilir",
+      question: t(
+        "faq.items.web-sitesi-analizi-kapsaminda-neler-yapilir.question"
+      ),
+      answer: t("faq.items.web-sitesi-analizi-kapsaminda-neler-yapilir.answer"),
+    },
+    {
+      key: "web-sitesi-analizi-hangi-araclarla-yapilir",
+      question: t(
+        "faq.items.web-sitesi-analizi-hangi-araclarla-yapilir.question"
+      ),
+      answer: t("faq.items.web-sitesi-analizi-hangi-araclarla-yapilir.answer"),
+    },
+    {
+      key: "web-sitesi-analizinin-faydalari",
+      question: t("faq.items.web-sitesi-analizinin-faydalari.question"),
+      answer: t("faq.items.web-sitesi-analizinin-faydalari.answer"),
+    },
+    {
+      key: "icerik-stratejisi-nedir",
+      question: t("faq.items.icerik-stratejisi-nedir.question"),
+      answer: t("faq.items.icerik-stratejisi-nedir.answer"),
+    },
+    {
+      key: "icerik-stratejisi-neden-onemlidir",
+      question: t("faq.items.icerik-stratejisi-neden-onemlidir.question"),
+      answer: t("faq.items.icerik-stratejisi-neden-onemlidir.answer"),
+    },
+    {
+      key: "hangi-tür-icerikler-uretiyorsunuz",
+      question: t("faq.items.hangi-tür-icerikler-uretiyorsunuz.question"),
+      answer: t("faq.items.hangi-tür-icerikler-uretiyorsunuz.answer"),
+    },
+    {
+      key: "icerikler-seo-uyumlu-mu-olacak",
+      question: t("faq.items.icerikler-seo-uyumlu-mu-olacak.question"),
+      answer: t("faq.items.icerikler-seo-uyumlu-mu-olacak.answer"),
+    },
+    {
+      key: "sosyal-medya-yonetimi-nedir",
+      question: t("faq.items.sosyal-medya-yonetimi-nedir.question"),
+      answer: t("faq.items.sosyal-medya-yonetimi-nedir.answer"),
+    },
+    {
+      key: "hangi-sosyal-medya-platformlarinda-hizmet-veriyorsunuz",
+      question: t(
+        "faq.items.hangi-sosyal-medya-platformlarinda-hizmet-veriyorsunuz.question"
+      ),
+      answer: t(
+        "faq.items.hangi-sosyal-medya-platformlarinda-hizmet-veriyorsunuz.answer"
+      ),
+    },
+    {
+      key: "sosyal-medya-yonetiminin-faydalari",
+      question: t("faq.items.sosyal-medya-yonetiminin-faydalari.question"),
+      answer: t("faq.items.sosyal-medya-yonetiminin-faydalari.answer"),
+    },
+    {
+      key: "sosyal-medya-stratejiniz-nedir",
+      question: t("faq.items.sosyal-medya-stratejiniz-nedir.question"),
+      answer: t("faq.items.sosyal-medya-stratejiniz-nedir.answer"),
+    },
+    {
+      key: "dijital-pazarlama-nedir",
+      question: t("faq.items.dijital-pazarlama-nedir.question"),
+      answer: t("faq.items.dijital-pazarlama-nedir.answer"),
+    },
+    {
+      key: "dijital-pazarlama-hizmetleri-nelerdir",
+      question: t("faq.items.dijital-pazarlama-hizmetleri-nelerdir.question"),
+      answer: t("faq.items.dijital-pazarlama-hizmetleri-nelerdir.answer"),
+    },
+    {
+      key: "dijital-pazarlama-calismalarinin-faydalari",
+      question: t(
+        "faq.items.dijital-pazarlama-calismalarinin-faydalari.question"
+      ),
+      answer: t("faq.items.dijital-pazarlama-calismalarinin-faydalari.answer"),
+    },
+    {
+      key: "dijital-pazarlama-stratejiniz-nedir",
+      question: t("faq.items.dijital-pazarlama-stratejiniz-nedir.question"),
+      answer: t("faq.items.dijital-pazarlama-stratejiniz-nedir.answer"),
     },
   ];
 
+  useEffect(() => {
+    const hash = location.hash.replace("#", "");
+    const questionIndex = faqItems.findIndex((item) => item.key === hash);
+    if (questionIndex !== -1) {
+      setOpenIndex(questionIndex);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.hash]);
+
   const toggleQuestion = (index) => {
-    setOpenIndex(openIndex === index ? null : index);
+    const newIndex = openIndex === index ? null : index;
+    setOpenIndex(newIndex);
+
+    if (newIndex !== null) {
+      navigate(`#${faqItems[index].key}`);
+    } else {
+      navigate("");
+    }
   };
 
   return (
